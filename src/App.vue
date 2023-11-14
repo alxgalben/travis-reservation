4c44bbc452a657fc7554fb43ceb9a8547dbb9fd5
alxgalben <galbenalex2@gmail.com>
2023-11-14 15:32:52 +0200

    fixes

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
   <div id="app">
+
+    <!-- MAIN CARD -->
+
+
     <div class="card main w-25 p-3">
       <div class="card-body">
         <h5 class="card-title">Personal Data</h5>
@@ -9,12 +13,14 @@
             <div class="row">
               <div class="col">
                 <label for="firstName">First Name</label>
-                <input type="text" class="form-control" id="firstName" placeholder="Enter your first name" v-model="formData.firstName" :class="{ 'is-invalid': !validateFirstName() }">
+                <input type="text" class="form-control" id="firstName" placeholder="Enter your first name"
+                  v-model="formData.firstName" :class="{ 'is-invalid': !validateFirstName() }">
                 <div class="invalid-feedback">Please enter a valid first name.</div>
               </div>
               <div class="col">
-                <label for="surname">Last Name</label>
-                <input type="text" class="form-control" id="lastName" placeholder="Enter your last name" v-model="formData.lastName" :class="{ 'is-invalid': !validateLastName() }">
+                <label for="lastName">Last Name</label>
+                <input type="text" class="form-control" id="lastName" placeholder="Enter your last name"
+                  v-model="formData.lastName" :class="{ 'is-invalid': !validateLastName() }">
                 <div class="invalid-feedback">Please enter a valid last name.</div>
               </div>
             </div>
@@ -22,7 +28,8 @@
             <div class="row">
               <div class="col">
                 <label for="exampleInputEmail1">Email address</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter your email" v-model="formData.email" :class="{ 'is-invalid': !validateEmail() }">
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                  placeholder="Enter your email" v-model="formData.email" :class="{ 'is-invalid': !validateEmail() }">
                 <div class="invalid-feedback">Please enter a valid email address.</div>
               </div>
               <div class="col">
@@ -36,7 +43,8 @@
             <div class="row">
               <div class="col">
                 <label for="phone">Phone</label>
-                <input type="tel" class="form-control" id="phone" placeholder="Enter your phone number" v-model="formData.phone" :class="{ 'is-invalid': !validatePhoneNumber() }">
+                <input type="tel" class="form-control" id="phone" placeholder="Enter your phone number"
+                  v-model="formData.phone" :class="{ 'is-invalid': !validatePhoneNumber() }">
                 <div class="invalid-feedback">Please enter a valid phone number.</div>
               </div>
               <div class="col">
@@ -63,7 +71,6 @@
                   <option value="7">Direct access to the site</option>
                   <option value="8">I am a loyal customer</option>
                   <option value="9">Another source</option>
-                  <!-- Add other options -->
                 </select>
               </div>
             </div>
@@ -75,13 +82,15 @@
             </div>
 
             <div class="alert alert-warning" role="alert">
-              Make the full payment now and get a 5% DISCOUNT of the rental rate ont he spot! Use the "PAYNOW" coupon to get the discount.
+              Make the full payment now and get a 5% DISCOUNT of the rental rate on the spot! Use the "PAYNOW" coupon to
+              get the discount.
             </div>
 
             <div class="input-group mb-3">
-              <input type="text" class="form-control" placeholder="Do you have a voucher?" aria-label="Do you have a voucher?" aria-describedby="basic-addon2">
+              <input type="text" class="form-control" id="voucher-code-input" placeholder="Do you have a voucher?"
+                aria-label="Do you have a voucher?" aria-describedby="basic-addon2">
               <div class="input-group-append">
-                <button class="btn btn-outline-warning" type="button">Apply discount!</button>
+                <button class="btn btn-outline-warning" id="btn-discount" type="button">Apply discount!</button>
               </div>
             </div>
           </form>
@@ -90,45 +99,89 @@
       </div>
     </div>
 
+
+    <!-- END OF MAIN CARD -->
+
+
+    <!-- SECOND CARD -->
+
+
     <div class="card another w-75 p-3">
       <div class="card-body" style="text-align: center;">
         <div class="alert alert-warning" role="alert" style="text-align: center;">
           <h1>THE FINAL PRICE WILL BE:</h1>
         </div>
-        <h1 style="font-size: 70px;"><span class="strikethrough">144 €</span></h1>
-          <h1 style="font-size: 200px;">123 €</h1>
+        <div class="big-text" style="margin-top: 50px;">
+          <h1 style="font-size: 40px;"><span class="strikethrough">144 €</span></h1>
+          <h1 style="font-size: 100px;">123 €</h1>
         </div>
-    <div class="row rowcard" style="margin-bottom: 20px;">
+      </div>
+      <div class="row rowcard" style="margin-bottom: 20px;">
         <div class="col-sm-4">
-            <div class="card" style="text-align: center;">
-                <div class="card-body">
-                    <h1><p class="card-text">0 €</p></h1>
-                    <input type="radio" class="btn-check" name="payment-option" id="btn-check-0" autocomplete="off">
-                    <label class="btn btn-outline-warning" for="btn-check-0" style="margin-top: 15px;">I WILL PAY LATER!</label><br>
-                </div>
+          <div class="card" style="text-align: center;">
+            <div class="card-body">
+              <h1>
+                <p class="card-text">0 €</p>
+              </h1>
+              <input type="radio" class="btn-check" name="payment-option" id="btn-check-0" autocomplete="off">
+              <label class="btn btn-outline-warning" for="btn-check-0" style="margin-top: 15px;">I WILL PAY
+                LATER!</label><br>
             </div>
-        </div>
-        <div class="col-sm-4">
-            <div class="card" style="text-align: center;">
-                <div class="card-body">
-                    <h1><p class="card-text">50 €</p></h1>
-                    <input type="radio" class="btn-check" name="payment-option" id="btn-check-50" autocomplete="off">
-                    <label class="btn btn-outline-warning" for="btn-check-50" style="margin-top: 15px;">I PAY PART NOW!</label><br>
-                </div>
-            </div>
+          </div>
         </div>
         <div class="col-sm-4">
-            <div class="card" style="text-align: center;">
-                <div class="card-body">
-                    <h1><p class="card-text">123 €</p></h1>
-                    <input type="radio" class="btn-check" name="payment-option" id="btn-check-123" autocomplete="off">
-                    <label class="btn btn-outline-warning" for="btn-check-123" style="margin-top: 15px;">I PAY FULL NOW!</label><br>
-                </div>
+          <div class="card" style="text-align: center;">
+            <div class="card-body">
+              <h1>
+                <p class="card-text">50 €</p>
+              </h1>
+              <input type="radio" class="btn-check" name="payment-option" id="btn-check-50" autocomplete="off">
+              <label class="btn btn-outline-warning" for="btn-check-50" style="margin-top: 15px;">I PAY PART
+                NOW!</label><br>
             </div>
+          </div>
         </div>
+        <div class="col-sm-4">
+          <div class="card" style="text-align: center;">
+            <div class="card-body">
+              <h1>
+                <p class="card-text">123 €</p>
+              </h1>
+              <input type="radio" class="btn-check" name="payment-option" id="btn-check-123" autocomplete="off">
+              <label class="btn btn-outline-warning" for="btn-check-123" style="margin-top: 15px;">I PAY FULL
+                NOW!</label><br>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="form-check form-check">
+        <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
+        <label class="form-check-label" for="inlineCheckbox1">
+          I agree with <a href="https://rentacar.travis.ro/en/car-rental-terms-and-conditions" target="_blank"> booking
+            terms and conditions </a> and <a href="https://rentacar.travis.ro/en/privacy-policy" target="_blank"> privacy
+            policy </a>
+        </label>
+      </div>
+      <div class="form-check form-check">
+        <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
+        <label class="form-check-label" for="inlineCheckbox2">
+          I want to create an account on rentacar.travis.ro
+        </label>
+      </div>
+      <div class="form-check form-check">
+        <input class="form-check-input" type="checkbox" id="inlineCheckbox3" value="option3">
+        <label class="form-check-label" for="inlineCheckbox3">
+          I want to be the first to find out about special offers and benefits offered by Travis Rent a Car
+        </label>
+      </div>
+
+      <button type="submit" class="btn btn-warning" style="font-weight: bold; margin-bottom: 20px;">COMPLETE YOU
+        BOOKING</button>
     </div>
-    <button type="submit" class="btn btn-warning" style="font-weight: bold; margin-bottom: 20px;">COMPLETE YOU BOOKING</button>
-</div>
+
+
+    <!-- END OF SECOND CARD -->
 
 
   </div>
@@ -188,16 +241,17 @@ export default {
   margin: 0;
 }
 
-.main{
+.main {
   margin-left: 50px;
   min-width: 550px;
   height: 85vh;
+  overflow-y: auto;
 }
 
-card-title {
-    font-size: 40px;
-    margin-bottom: 20px;
-  }
+.card-title {
+  font-size: 40px;
+  margin-bottom: 20px;
+}
 
 .form-group {
   margin: 10px 0;
@@ -208,7 +262,7 @@ card-title {
 }
 
 .row {
-  margin-top: 20px;
+  margin-top: 5px;
 }
 
 label {
@@ -223,24 +277,29 @@ label {
   margin: 0 50px;
   height: 85vh;
   min-width: none;
+  overflow-y: auto;
 }
 
 .strikethrough {
   position: relative;
 }
+
 .strikethrough:before {
   position: absolute;
   content: "";
   left: 0;
   top: 50%;
   right: 0;
-  border-top: 5px solid;
+  border-top: 3px solid;
   border-color: inherit;
-  
-  -webkit-transform:rotate(-5deg);
-  -moz-transform:rotate(-5deg);
-  -ms-transform:rotate(-5deg);
-  -o-transform:rotate(-5deg);
-  transform:rotate(-5deg);
+
+  -webkit-transform: rotate(-20deg);
+  -moz-transform: rotate(-20deg);
+  -ms-transform: rotate(-20deg);
+  -o-transform: rotate(-20deg);
+  transform: rotate(-20deg);
 }
-</style>
+
+#btn-discount {
+  border-radius: 0 5px 5px 0;
+}</style>
